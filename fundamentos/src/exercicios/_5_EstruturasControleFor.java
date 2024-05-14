6a819ef927b028c4ac9f268f7f84f4d4a2ef2399
fabiana.s@gmail.com <fabiana.s@gmail.com>
2024-05-14 15:07:16 -0300

    Pilares OO - Livraria

--- fundamentos/src/exercicios/_5_EstruturasControleFor.java
+++ fundamentos/src/exercicios/_5_EstruturasControleFor.java
@@ -3,10 +3,18 @@ package exercicios;
 public class _5_EstruturasControleFor {
 	public static void main(String[] args) {
 		
-		int x = 0;
-		
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Loop for");
+	
+			
+			 int sum = 0;
+
+		        // Loop iterates from 2 to 20 (inclusive) with a step of 2
+		        for (int i = 2; i <= 20; i += 2) {
+		            System.out.println(i);
+		            sum += i;
+		        }
+
+		        System.out.println("Soma dos números pares: " + sum);
+		    }
 		}
-	}
-}
+	
+
